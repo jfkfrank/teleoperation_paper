f62542c2de3f24aa243f9d6c62b37be1b737f831
Francisco Javier Rodriguez Lera <fjrodl@unileon.es>
2016-04-18 01:58:15 +0200

    CLeaning logs files. Updated paper title

--- conferences/WAF_2016/Seguridad/data/Images/Cifrador/dstat_csv_statistics.py
+++ conferences/WAF_2016/Seguridad/data/Images/Cifrador/dstat_csv_statistics.py
@@ -10,8 +10,31 @@ import csv
 import sys
 import getopt
 
+
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+from Tkinter import *
+import ttk
+
 DEBUG = False
 
+
+class Checkbar(Frame):
+   def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
+      Frame.__init__(self, parent)
+      self.vars = []
+      for pick in picks:
+         var = IntVar()
+         chk = Checkbutton(self, text=pick, variable=var)
+         chk.pack(side=side, anchor=anchor, expand=YES)
+         self.vars.append(var)
+         
+   def state(self):
+      return map((lambda var: var.get()), self.vars)
+      
+      
+
+#class principal()
 def help():
 	print 'test.py -i <inputfile> -o <outputfile>'
 	sys.exit(2)
@@ -25,19 +48,24 @@ def main(argv):
 	inputfile = ''
 	outputfile = ''
 	try:
-	  opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+	  opts, args = getopt.getopt(argv,"hi:o:g",["ifile=","ofile="])
 	except getopt.GetoptError:
 		help()
 			  
 	for opt, arg in opts:
 	  if opt == '-h':
-		 print 'test.py -i <inputfile> -o <outputfile>'
-		 sys.exit()
+                print 'test.py -i <inputfile> -o <outputfile>'
+                sys.exit()
 	  elif opt in ("-i", "--ifile"):
-		 inputfile = arg
+                inputfile = arg
 	  elif opt in ("-o", "--ofile"):
-		 outputfile = arg
-		 
+                outputfile = arg
+          elif opt in ("-g", "--gui"):
+                Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+                inputfile = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+                print(inputfile)
+                
+                
 		 
 	print 'Input file is "', inputfile
 	print 'Output file is "', outputfile
@@ -50,7 +78,7 @@ def main(argv):
 		tmp = [i for i in v_reader]
 		#print([i for i in reader])  
 		
-	csvReader = csv.reader(open('fichero_salida_b1.csv'), delimiter=';')
+	csvReader = csv.reader(open(inputfile), delimiter=';')
 	headers1 = csvReader.next()
 	headers2 = csvReader.next()
 	values = [map(float, row) for row in csvReader]
@@ -64,11 +92,30 @@ def main(argv):
           print len(tmp)
           print type(values)
           print type(tmp)
-	 
-	 
+	
+	if DEBUG:
+          root = Tk()
+          lng = Checkbar(root, headers1)
+          tgl = Checkbar(root, headers2)
+          lng.pack(side=TOP,  fill=X)
+          tgl.pack(side=LEFT)
+          lng.config(relief=GROOVE, bd=2)
+
+          def allstates(): 
+              print(list(lng.state()), list(tgl.state()))
+            
+          Button(root, text='Quit', command=root.quit).pack(side=RIGHT)
+          Button(root, text='Accept', command=allstates).pack(side=RIGHT)
+        
+          root.mainloop()
+          
 	for index,item in enumerate(headers2):
 	  try:
-		
+		if headers1[index] != '':
+                  print "*****************************************************"
+                  print headers1[index]
+                  print "*****************************************************"
+                  
 		print item 
 		element = [float(x[index]) for x in tmp]
 		desc = ss.describe(element)
